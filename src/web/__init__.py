b13f7dcef76a25f210134d6330a25c24b69ee6a0
Quan Truong Pham <phamtruongquan@iuh.edu.vn>
2026-07-30 21:51:37 +0700

    feat: install answer backup restore routes

--- src/web/__init__.py
+++ src/web/__init__.py
@@ -1,3 +1,12 @@
-from src.web.app import app
+from src.web.app import app, get_config, get_db_and_engine, save_config
+from src.web.answer_backup_routes import install_answer_backup_routes
+
+
+install_answer_backup_routes(
+    app,
+    get_config=get_config,
+    save_config=save_config,
+    get_db_and_engine=get_db_and_engine,
+)
 
 __all__ = ["app"]
